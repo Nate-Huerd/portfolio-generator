23ec19421d9dffe1d9c10796b1cec2022f04fbc1
Nate-Huerd <nhuerd3@gmail.com>
2022-01-19 18:58:06 -0600

    end of 9.2

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+/* Commented out per instructions on 9.2.3
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-    console.log('================');
+  console.log('================');
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+
+printProfileData(profileDataArgs);*/
